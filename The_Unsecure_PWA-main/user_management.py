daac49ab00a7411fc5da27b6c8b3f54ac7610564
WarmTheAviator <derick.dao1@education.nsw.gov.au>
2025-04-06 15:11:39 +1000

    Update user_management.py
    insertFeedbackupdated

--- The_Unsecure_PWA-main/user_management.py
+++ The_Unsecure_PWA-main/user_management.py
@@ -42,7 +42,10 @@ def retrieveUsers(username, password):
 def insertFeedback(feedback):
     con = sql.connect("database_files/database.db")
     cur = con.cursor()
-    cur.execute("INSERT INTO feedback (feedback) VALUES (?)", (feedback,))
+    f=filter(str.isdecimal,feedback)
+    s1="".join(f)
+    print(s1)
+    cur.execute(f"INSERT INTO feedback (feedback) VALUES ('{s1}')")
     con.commit()
     con.close()
 
